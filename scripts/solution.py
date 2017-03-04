ef3d319f411d167704b75345d0a9a1657f6bc347
Donald Heppner <donald.heppner@gmail.com>
2017-03-04 09:38:57 -0500

    Just getting started

---
+++ scripts/solution.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+path = os.path.join("..", "tests", "data")
+
+for item in os.scandir(path):
+    if item.name.endswith(".in"):
+        in_path = os.path.join(path, item.name)
+        print(in_path)
+        answer_path = os.path.join(path, item.name[0:-3] + ".ans")
+        if os.path.isfile(answer_path):
+            print(answer_path)
+
---
+++ tests/solution_test.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+import os
+#__import__("..\\scripts\\solution.py")
+
+class SolutionTestCase(unittest.TestCase):
+    def test_sampledata(self):
+        path = os.path.join("data")
+
+        for item in os.scandir(path):
+            if item.name.endswith(".in"):
+                in_path = os.path.join(path, item.name)
+                answer_path = os.path.join(path, item.name[0:-3] + ".ans")
+                if os.path.isfile(answer_path):
+                    print(answer_path)
+                    self.assertTrue(self,"wee!")
+
+
+if __name__ == '__main__':
+    unittest.main()
