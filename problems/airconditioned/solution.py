a26b97fdbd2c88669b34150ecaaa3be7f2ce8532
Donald Heppner <donald.heppner@gmail.com>
2017-03-07 18:01:48 -0500

    Initialized airconditioned

---
+++ problems/airconditioned/solution.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    input = sys.stdin
+    output = sys.stdout
+
+    solve(input, output)
+
+    input.flush()
+    output.flush()
+
+
+def solve(input, output):
+    output.write("hello!")
+
+
+if __name__ == '__main__':
+    main()
