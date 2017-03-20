cf2a9b48ad8129c885369f8b7497afc8cd0129bc
Donald Heppner <donald.heppner@gmail.com>
2017-03-19 22:48:15 -0400

    wiiiiiiiiiiiip

--- problems/minflow/loop.py
+++ problems/minflow/loop.py
@@ -19,23 +19,26 @@ while len(stack) > 0:
 stack = [0]
 target = 3
 
-blah = [x for x in range(0, 100)]
-blahblah = itertools.permutations(blah)
-for x in blahblah:
-    print(x)
-
 # while len(stack) > 0:
-#     if stack[-1] == target:
-#         print(stack, sep=' ', end='\n')
-#
-#     # for next_junction in [x for x in all if x not in path and current.can_connect(x, path)]:
-#     #     costs.append(find_min_cost(next_junction, target, all, path.copy()))
-#
-#
 #     for x in [x for x in range(0, target) if x not in stack]:
 #         stack.append(x)
+#         print(stack)    # do something
+#     else:
+#         while len(stack) > 0 and stack[-1] == target - 1 and stack[-1] + 1 in stack:
+#             stack.pop()
+#         else:
+#             stack[-1] += 1
 #
-#     for x in range(0, target):
-#         if x not in stack:
-#             stack.append(x)
-#             break
+
+input = [x for x in range(0, 4)]
+factorials = [0] * (len(input) + 1)
+factorials[0] = 1
+
+for i in range(1, len(input) + 1):
+    factorials[i] = factorials[i-1] * i
+
+print(factorials)
+
+    # for next_junction in [x for x in all if x not in path and current.can_connect(x, path)]:
+    #     costs.append(find_min_cost(next_junction, target, all, path.copy()))
+
