3df10831c6a0c5470c72a1b7720737b17520d55e
Donald Heppner <donald.heppner@gmail.com>
2017-12-27 20:09:30 -0500

    solves 10 kinds of people

--- problems/10kindsofpeople/solution.py
+++ problems/10kindsofpeople/solution.py
@@ -11,21 +11,28 @@ def main():
     output.flush()
 
 
-class Key:
-    def __init__(self, value):
-        self.value = value
+def in_region(r1, r2, map, visited):
+    if r1 == r2:
+        return True
+    elif r1 in map:
+        nv = visited.copy()
+        nv.add(r1)
+        for r in (map[r1] - nv):
+            if in_region(r, r2, map, nv):
+                return True
+        else:
+            return False
+    else:
+        return False
 
-    def __str__(self):
-        return str(self.value)
 
+def get_region(r1, map, visited):
+    visited.add(r1)
+    if r1 in map:
+        for r in (map[r1] - visited):
+            get_region(r, map, visited)
 
-class Region:
-    def __init__(self, id):
-        self.id = Key(id)
-
-    def __str__(self):
-        return str(self.id.value)
-
+    return visited
 
 def solve(input, output):
     (rows, columns) = map(int, input.readline().split())
@@ -37,31 +44,50 @@ def solve(input, output):
     for row in range(0, rows):
         lines.append(input.readline())
 
+    equivalents = dict()
+
     for row in range(0, rows):
         for column in range(0, columns):
             val = lines[row][column]
             if row > 0 and lines[row - 1][column] == val:  # if top matches val, then we're the same region
                 region_map[row][column] = region_map[row - 1][column]
-                # if back also matches on value but they do not share an id, then merge regions by
-                # assigning current and back the min_id
-                if lines[row][column - 1] == val and region_map[row][column - 1].id.value != region_map[row][column].id.value:
-                    min_id = region_map[row][column - 1].id if region_map[row][column - 1].id.value < region_map[row][column].id.value else region_map[row][column].id
-                    region_map[row][column].id = min_id
-                    region_map[row][column - 1].id = min_id
+                # if back also matches on value but they do not share an id, then map the regions as equivalent
+                if lines[row][column - 1] == val and region_map[row][column - 1] != region_map[row][column]:
+                    a = region_map[row][column - 1]
+                    b = region_map[row][column]
+                    if a in equivalents:
+                        equivalents[a].add(b)
+                    else:
+                        equivalents[a] = set([b])
+
+                    if b in equivalents:
+                        equivalents[b].add(a)
+                    else:
+                        equivalents[b] = set([a])
             elif column > 0 and lines[row][column - 1] == val:  # if back matches val, then we're the same region
                 region_map[row][column] = region_map[row][column - 1]
             else:  # new region
                 region_count += 1
-                region_map[row][column] = Region(region_count)
+                region_map[row][column] = region_count
+
+    # simplify the region map
+    regions = [None] * (region_count + 1)
+    for r in range(1, len(regions)):
+        if regions[r] is None:
+            s = get_region(r, equivalents, set())
+            for x in s:
+                regions[x] = s
 
     num_queries = int(input.readline())
     for x in range(0, num_queries):
         (r1, c1, r2, c2) = map(int, input.readline().split())
         region1 = region_map[r1 - 1][c1 - 1]
         region2 = region_map[r2 - 1][c2 - 1]
-        if region1.id.value == region2.id.value:
+        #if in_region(region1, region2, equivalents, set()):
+        if region1 == region2 or region2 in regions[region1]:
             output.write("binary" if lines[r1 - 1][c1 - 1] == '0' else "decimal")
         else:
+            # traverse equivalents map
             output.write("neither")
         output.write("\n")
 
